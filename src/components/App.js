1fb997953283ef90a14cce40c00d369552afbdda
ndevu3399 <wachiramwas122@gmail.com>
2025-04-18 21:11:40 +0300

    modified app.js

--- src/components/App.js
+++ src/components/App.js
@@ -16,15 +16,23 @@ function Home() {
   );
 }
 
-{/* write an <About> component here */}
-
+// ✅ Define the <About> component here
+function About() {
+  return (
+    <div id="about">
+      <h2>About</h2>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      {/* ✅ Add the <Home> component here */}
+      <Home />
+      {/* ✅ Add the <About> component here */}
+      <About />
     </div>
   );
 }
